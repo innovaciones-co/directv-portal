591f6f447d93b5f8c0d193eca833fd0c2ad346c2
Sebastian Cepeda <sebastian.camilocepeda@hotmail.com>
2025-02-21 12:16:15 -0500

    se termina el header

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
+  menuOpen: boolean = false;
+  dropdownStates: { [key: string]: boolean } = {};
 
+  toggleMenu(event: Event): void {
+    event.stopPropagation();
+    this.menuOpen = !this.menuOpen;
+  }
+
+  toggleDropdown(key: string, event: Event): void {
+    event.preventDefault();
+    event.stopPropagation();
+    this.dropdownStates[key] = !this.dropdownStates[key];
+  }
+
+  @HostListener('document:click', ['$event'])
+  onDocumentClick(event: Event): void {
+    this.menuOpen = false;
+    this.dropdownStates = {};
+  }
 }
