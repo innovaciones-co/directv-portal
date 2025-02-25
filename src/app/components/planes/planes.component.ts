3cc09cc33fd3fd6eb6b72a938ef14cda7e69518b
Sebastian Cepeda <sebastian.camilocepeda@hotmail.com>
2025-02-25 09:54:57 -0500

    Se muestran los planes en forma de carrusel

--- src/app/components/planes/planes.component.ts
+++ src/app/components/planes/planes.component.ts
@@ -1,14 +1,14 @@
-import { Component,OnInit} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ProductOfferingService } from '../../services/product-offering.service';
-
 
 @Component({
   selector: 'app-planes',
   templateUrl: './planes.component.html',
-  styleUrl: './planes.component.scss'
+  styleUrls: ['./planes.component.scss']
 })
-export class PlanesComponent  implements OnInit {
+export class PlanesComponent implements OnInit {
   plans: any[] = [];
+  currentIndex: number = 0;
 
   constructor(private productService: ProductOfferingService) {}
 
@@ -16,5 +16,29 @@ export class PlanesComponent  implements OnInit {
     this.productService.getProductOffering().subscribe(data => {
       this.plans = data.plans;
     });
+  }
+
+  nextSlide(): void {
+    if (this.plans.length > 0) {
+      this.currentIndex = (this.currentIndex + 1) % this.plans.length;
+    }
+  }
+
+  prevSlide(): void {
+    if (this.plans.length > 0) {
+      this.currentIndex = (this.currentIndex - 1 + this.plans.length) % this.plans.length;
+    }
+  }
+
+  getClass(i: number): string {
+    if (i === this.currentIndex) {
+      return 'active';
+    } else if (i === (this.currentIndex - 1 + this.plans.length) % this.plans.length) {
+      return 'prev';
+    } else if (i === (this.currentIndex + 1) % this.plans.length) {
+      return 'next';
+    } else {
+      return 'hidden';
+    }
   }
 }
