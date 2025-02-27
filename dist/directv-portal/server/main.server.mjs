fc5842b8b956cd96aacaa7034c48f74778cdf3e9
Sebastian Cepeda <sebastian.camilocepeda@hotmail.com>
2025-02-27 15:34:48 -0500

    se corrige responsive

--- dist/directv-portal/server/main.server.mjs
+++ dist/directv-portal/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-J2BUMO55.mjs";import"./chunk-RY2T7RQP.mjs";import"./chunk-KBFMT6SZ.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
+import{a}from"./chunk-RGPJDV6V.mjs";import"./chunk-R752GNFC.mjs";import"./chunk-KBFMT6SZ.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
