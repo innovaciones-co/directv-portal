379ba6efafa192793bec011c51ec08e327d2e6d3
sebastiancepe <sebastian.camilocepeda@hotmail.com>
2025-03-03 10:29:05 -0500

    se crea modulo de homologation y se corrige oferta CRC

---
+++ src/app/components/homologation/homologation.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-homologation',
+  templateUrl: './homologation.component.html',
+  styleUrl: './homologation.component.scss'
+})
+export class HomologationComponent {
+
+}
