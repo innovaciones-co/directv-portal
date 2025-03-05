5a054527d4ebc40cdfdeb7bfbdd9983802ecfde9
Sebastian Cepeda <sebastian.camilocepeda@hotmail.com>
2025-03-05 17:08:50 -0500

    se corrige redireccion de header DTV y la informacion de footer

--- dist/directv-portal/server/main.server.mjs
+++ dist/directv-portal/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-NXLUGC7N.mjs";import"./chunk-R752GNFC.mjs";import"./chunk-KBFMT6SZ.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
+import{a}from"./chunk-4RNQSYGZ.mjs";import"./chunk-ZEIG2JGR.mjs";import"./chunk-2IKSPRXL.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
